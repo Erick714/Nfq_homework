7b4273c672c837d13cbf82c10bfddd9c44f3e393
Erick714 <Ernest.caiko@gmail.com>
2017-02-11 15:29:09 +0200

    basics
    added search tool

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$conn = mysqli_connect("sql7.freemysqlhosting.net", "sql7153047", "2TKkMMw3Gv", "sql7153047"  ) ;
+
+if(!$conn){
+	die("connection failed: ");
+	echo "error";
+}
+
+?>
--- index.php
+++ index.php
@@ -3,8 +3,70 @@
   <head>
     <meta charset="utf-8">
     <title>NFQ Homework</title>
+
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
   </head>
   <body>
+<nav class="fixed teal">
+<div class="row container">
+  <form class="" action="index.php" method="GET">
+
+          <div class="input-field col s4">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" class="validate" name="value">
+            <label for="icon_prefix">Search</label>
+          </div>
+          <div class="input-field col s2">
+              <select name="search_by">
+                <option value="0" disabled selected>search by</option>
+                <option value="1">Name</option>
+                <option value="2">Autor</option>
+              </select>
+          </div>
+          <div class="input-field col s2">
+              <select name="sort_by">
+                <option value="0" disabled selected>Sort by</option>
+                <option value="1">Name</option>
+                <option value="2">Autor</option>
+                <option value="3">year</option>
+              </select>
+          </div>
+
+
+          <div class="switch">
+            <label>
+              <i class="material-icons prefix inline">swap_vert</i>
+              <input type="checkbox">
+              <span class="lever"></span>
+              On
+            </label>
+          </div>
+
+
+</div>
+
+
+            <div class="col s2">
+                <input type="submit">
+            </div>
+
+  </form>
+</div>
+
+</nav>
+
+
+
+
+
+
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+        <script src="js/init.js"></script>
+
 
   </body>
 </html>
